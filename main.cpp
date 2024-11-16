bbd9adc0376508d36a838d023a2b8b46094dbf88
Kainoa Gardner <kainoagardner123@gmail.com>
2024-11-16 01:32:03 -0800

    3d and ascii done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <iostream>
 #include <stdlib.h>
+#include <string>
 #include <tuple>
 #include <unistd.h>
 #include <vector>
@@ -17,13 +18,32 @@ public:
     z = startZ;
   }
 
-  std::tuple<float, float> updatePoint(float angle) {
-    // temp 2d
+  std::tuple<float, float, float> updatePoint(float xAngle, float yAngle,
+                                              float zAngle) {
+    int newX = x;
+    int newY = y;
+    int newZ = z;
 
-    int newX = x * cos(angle) - y * sin(angle);
-    int newY = x * sin(angle) + y * cos(angle);
+    // x axis
+    if (xAngle) {
+      newX = x;
+      newY = y * cos(xAngle) - z * sin(xAngle);
+      newZ = y * sin(xAngle) + z * cos(xAngle);
+    }
 
-    return std::make_tuple(newX, newY);
+    if (yAngle) {
+      newX = x * cos(yAngle) + z * sin(yAngle);
+      newY = y;
+      newZ = z * cos(yAngle) - x * sin(yAngle);
+    }
+
+    if (zAngle) {
+      newX = x * cos(zAngle) - y * sin(zAngle);
+      newY = x * sin(zAngle) + y * cos(zAngle);
+      newZ = z;
+    }
+
+    return std::make_tuple(newX, newY, newZ);
   };
 };
 
@@ -31,53 +51,19 @@ int main() {
   int m = 50;
   int n = 50;
   int FPS = 30;
-  int angle = 0;
+  int xAngle = 0;
+  int yAngle = 0;
+  int zAngle = 0;
+
+  std::string brightness = ",,..++::vvoo00XXXXPPPP####BBBB@@@@";
 
   std::vector<Point> points = {};
 
-  points.push_back(Point(5, -5, 0));
-  points.push_back(Point(5, -4, 0));
-  points.push_back(Point(5, -3, 0));
-  points.push_back(Point(5, -2, 0));
-  points.push_back(Point(5, -1, 0));
-  points.push_back(Point(5, 0, 0));
-  points.push_back(Point(5, 1, 0));
-  points.push_back(Point(5, 2, 0));
-  points.push_back(Point(5, 3, 0));
-  points.push_back(Point(5, 4, 0));
-  points.push_back(Point(5, 5, 0));
+  points.push_back(Point(15, -15, 0));
+  points.push_back(Point(15, 15, 0));
 
-  points.push_back(Point(-5, -5, 0));
-  points.push_back(Point(-5, -4, 0));
-  points.push_back(Point(-5, -3, 0));
-  points.push_back(Point(-5, -2, 0));
-  points.push_back(Point(-5, -1, 0));
-  points.push_back(Point(-5, 0, 0));
-  points.push_back(Point(-5, 1, 0));
-  points.push_back(Point(-5, 2, 0));
-  points.push_back(Point(-5, 3, 0));
-  points.push_back(Point(-5, 4, 0));
-  points.push_back(Point(-5, 5, 0));
-
-  points.push_back(Point(-4, 5, 0));
-  points.push_back(Point(-3, 5, 0));
-  points.push_back(Point(-2, 5, 0));
-  points.push_back(Point(-1, 5, 0));
-  points.push_back(Point(0, 5, 0));
-  points.push_back(Point(1, 5, 0));
-  points.push_back(Point(2, 5, 0));
-  points.push_back(Point(3, 5, 0));
-  points.push_back(Point(4, 5, 0));
-
-  points.push_back(Point(-4, -5, 0));
-  points.push_back(Point(-3, -5, 0));
-  points.push_back(Point(-2, -5, 0));
-  points.push_back(Point(-1, -5, 0));
-  points.push_back(Point(0, -5, 0));
-  points.push_back(Point(1, -5, 0));
-  points.push_back(Point(2, -5, 0));
-  points.push_back(Point(3, -5, 0));
-  points.push_back(Point(4, -5, 0));
+  points.push_back(Point(-15, -15, 0));
+  points.push_back(Point(-15, 15, 0));
 
   int **grid = new int *[m];
 
@@ -92,29 +78,37 @@ int main() {
 
     for (int r = 0; r < m; r++) {
       for (int c = 0; c < n; c++) {
-        grid[r][c] = 0;
+        grid[r][c] = m * 2;
       }
     }
 
-    angle -= 5;
+    zAngle += 5;
 
     for (int i = 0; i < points.size(); i++) {
-      float radianAngle = (angle % 360) * M_PI / 180;
+      float radXAngle = (xAngle % 360) * M_PI / 180;
+      float radYAngle = (yAngle % 360) * M_PI / 180;
+      float radZAngle = (zAngle % 360) * M_PI / 180;
 
-      std::tuple<float, float> point = points[i].updatePoint(radianAngle);
+      std::tuple<float, float, float> point =
+          points[i].updatePoint(radXAngle, radYAngle, radZAngle);
 
       int pointX = (n / 2) + std::get<0>(point);
       int pointY = (m / 2) + std::get<1>(point);
+      int pointZ = std::get<2>(point);
 
       if (pointX >= 0 && pointX < n && pointY >= 0 && pointY < m) {
-        grid[pointY][pointX] = 1;
+        if (grid[pointY][pointX] == m * 2 || pointZ > grid[pointY][pointX]) {
+          grid[pointY][pointX] = pointZ;
+        }
       }
     }
 
     for (int r = 0; r < m; r++) {
       for (int c = 0; c < n; c++) {
-        if (grid[r][c] != 0) {
-          std::cout << "0";
+        if (grid[r][c] != m * 2) {
+          float percent = ((m / 2) + float(grid[r][c])) / m;
+          int result = percent * brightness.size();
+          std::cout << brightness[result];
         }
         std::cout << " ";
       }
